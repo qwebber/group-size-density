84e035f0580c8c99d15b1e596e8fa3e6aa1bb8e6
Quinn Webber <webber.quinn@gmail.com>
2021-03-04 16:56:01 -0330

    Update Fig3.R

--- scripts/Fig3.R
+++ scripts/Fig3.R
@@ -36,7 +36,71 @@ habitat2$HSize[habitat2$HERD == "St_Anthony"] <- "Med"
 habitat2$HSize[habitat2$HERD == "Lapoile"] <- "Large"
 habitat2$HSize[habitat2$HERD == "Middle_Ridge"] <- "Large"
 
-png("graphics/Fig3.png",width = 6000, height = 4500, res = 600)
+png("graphics/Fig3.png",width = 6000, height = 3000, res = 600)
+
+dd = ggplot(habitat2[HSize == "Small" & Total_Caribou < 100 & season != "Rut"], 
+            aes(size, Total_Caribou)) +
+  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
+  geom_smooth(method = "lm", color = "black") +
+  scale_fill_manual(values=c("#e08214", "#b2abd2")) +
+  scale_color_manual(values=c("#e08214", "#b2abd2")) +
+  scale_x_log10() +
+  xlab('Density') +
+  ylab('Group size') +
+  ggtitle('A)') +
+  theme(legend.position = 'none',
+        axis.title = element_text(size = 18),
+        axis.text.y = element_text(size=12, color = "black"),
+        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),       
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))
+
+ee = ggplot(habitat2[HSize == "Med" & Total_Caribou < 100 & season != "Rut"], 
+            aes(size, Total_Caribou)) +
+  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
+  geom_smooth(method = "lm", color = "black") +
+  scale_fill_manual(values=c("#e08214", "#b2abd2")) +
+  scale_color_manual(values=c("#e08214", "#b2abd2")) +
+  scale_x_log10() +
+  xlab('Density') +
+  ylab('Group size') +
+  ggtitle('B)') +
+  theme(legend.position = 'none',
+        axis.title = element_text(size = 18),
+        axis.text.y = element_text(size=12, color = "black"),
+        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),      
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))
+ff = ggplot(habitat2[HSize == "Large" & Total_Caribou < 100 & season != "Rut"], 
+            aes(size, Total_Caribou)) +
+  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
+  geom_smooth(method = "lm", color = "black") +
+  scale_fill_manual(values=c("#e08214", "#b2abd2")) +
+  scale_color_manual(values=c("#e08214", "#b2abd2")) +
+  scale_x_log10() + 
+  xlab('Density') +
+  ylab('Group size') +
+  ggtitle('C)') +
+  theme(legend.position = c(0.75,0.9),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12),
+        axis.title = element_text(size = 18),
+        axis.text.y = element_text(size=12, color = "black"),
+        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),      
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))
+
+grid.arrange(dd,ee,ff, ncol = 3, nrow = 1)
+
+dev.off()
+
+
+
 aa = ggplot(habitat2[HSize == "Small" & Total_Caribou < 100], 
             aes(habitat, Total_Caribou)) +
   geom_jitter(aes(color = habitat), alpha = 0.5, width = 0.04, size = 3) +
@@ -93,64 +157,3 @@ cc = ggplot(habitat2[HSize == "Large" & Total_Caribou < 100],
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=1))
-
-dd = ggplot(habitat2[HSize == "Small" & Total_Caribou < 100 & season != "Rut"], 
-            aes(size, Total_Caribou)) +
-  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  scale_fill_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_color_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_x_log10() +
-  xlab('Density') +
-  ylab('Group size') +
-  ggtitle('D)') +
-  theme(legend.position = 'none',
-        axis.title = element_text(size = 18),
-        axis.text.y = element_text(size=12, color = "black"),
-        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),       
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=1))
-
-ee = ggplot(habitat2[HSize == "Med" & Total_Caribou < 100 & season != "Rut"], 
-            aes(size, Total_Caribou)) +
-  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  scale_fill_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_color_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_x_log10() +
-  xlab('Density') +
-  ylab('Group size') +
-  ggtitle('E)') +
-  theme(legend.position = 'none',
-        axis.title = element_text(size = 18),
-        axis.text.y = element_text(size=12, color = "black"),
-        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),      
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=1))
-ff = ggplot(habitat2[HSize == "Large" & Total_Caribou < 100 & season != "Rut"], 
-            aes(size, Total_Caribou)) +
-  geom_jitter(aes(color = season), alpha = 0.5, width = 0.04, size = 3) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  scale_fill_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_color_manual(values=c("#7fbf7b", "#af8dc3")) +
-  scale_x_log10() + 
-  xlab('Density') +
-  ylab('Group size') +
-  ggtitle('F)') +
-  theme(legend.position = c(0.75,0.9),
-        legend.title = element_blank(),
-        legend.background = element_blank(),
-        legend.key = element_blank(),
-        legend.text = element_text(size = 12),
-        axis.title = element_text(size = 18),
-        axis.text.y = element_text(size=12, color = "black"),
-        axis.text.x = element_text(size = 12, angle = 45 , color = "black", hjust = 1),      
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=1))
-
-grid.arrange(aa,bb,cc,dd,ee,ff, ncol = 3, nrow = 2)
-
-dev.off()
